d6283ffe5319b560b576e1516fe4f739ba2c4729
denis <denis.silva@sunsetcom.com.br>
2018-08-22 19:05:28 -0300

    finalizando busca

--- public/script.js
+++ public/script.js
@@ -1,21 +1,74 @@
+var PRICE = 9.99;
+
+
+
 new Vue({
     el: '#app',
     data: {
         total: 0,
-        items: [
-            { title: 'Item 1' },
-            { title: 'Item 2' },
-            { title: 'Item 3' },
-        ],
-        cart: [
+        items: [],
+        cart: [],
+        newSearch: '',
+        lastSearch: '',
+        loading: false
 
-        ]
     },
     methods: {
+        onSubmit(){
+            this.items = [];
+            this.loading = true;
+            this.$http
+                .get('/search/'.concat(this.newSearch))
+                .then(function(res){
+                    this.items = res.data;
+                    this.loading = false;
+                    this.lastSearch = this.newSearch;
+                    this.newSearch = '';
+                })
+        },
         addItem(index){
-            this.total += 9.99;
-            this.cart.push(this.items[index])
-            console.log(this.cart)
+            this.total += PRICE;
+            // this.cart.push(this.items[index])
+            var item = this.items[index];
+            var found = false;
+            for(var i = 0; i < this.cart.length; i++){
+                if(this.cart[i].id == item.id){
+                    found = true
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+
+            if(!found){
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE
+                })
+            }
+         
+        },
+        inc(item){
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec(item){
+            item.qty--;
+            this.total -= PRICE;
+            if(item.qty <= 0){
+                for(var i = 0; i < this.cart.length; i++){
+                    if(this.cart[i].id === item.id){
+                        this.cart.splice(i, 1)
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function(price){
+            return '$'.concat(price.toFixed(2))
         }
     }
 })
